4bc9aff5cfb5ae359bb9ac7fdea26b19b524cb66
Pareder <ruskc2beat@gmail.com>
2025-04-10 17:12:59 +0300

    Add image optimizer plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,37 @@
 import vituum from 'vituum';
 import pug from '@vituum/vite-plugin-pug';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default {
   plugins: [
     vituum(),
     pug({
       root: './src',
+    }),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                removeViewBox: false,
+              },
+            },
+          },
+        ],
+      },
     }),
   ],
 };
